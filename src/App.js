c16ddd44bfa30ec07bc044f7b44b4dfe4c55a6aa
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-09-10 09:12:37 -0700

    Add style components next day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [nasaData, getNasaData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=HNLQulJo3avR2EshFeCHGKZyeXLr46GGjQbG9sLr&date=2020-08-13`
+      )
+      .then((response) => {
+        getNasaData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(nasaData);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="nasa-info">
+      {/* <Header title = {nasaData.title}/> */}
+      <h1>NASA Photo of the Day</h1>
+      <h2> Title: {nasaData.title} </h2>
+      <h3> Date Captured: {nasaData.date}</h3>
+      <img src={nasaData.url} alt="photo" />
+      <h4>Photo Information Below</h4>
+      <p> {nasaData.explanation} </p>
     </div>
   );
 }
