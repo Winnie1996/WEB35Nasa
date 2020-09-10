1c605c64f12972079903a9a948c3edcdfcdf8171
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-09-10 14:59:04 -0700

    Review Code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        `https://api.nasa.gov/planetary/apod?api_key=HNLQulJo3avR2EshFeCHGKZyeXLr46GGjQbG9sLr&date=2020-08-13`
+        `https://api.nasa.gov/planetary/apod?api_key=HNLQulJo3avR2EshFeCHGKZyeXLr46GGjQbG9sLr&date=2020-09-09`
       )
       .then((response) => {
         getNasaData(response.data);
@@ -18,15 +18,53 @@ function App() {
   }, []);
   console.log(nasaData);
 
+  const [infoData, setInfoData] = useState(true);
+  const toggle = () => {
+    if (infoData === false) {
+      const btnInfo = document.getElementById("explanation");
+      btnInfo.style.display = "none";
+    } else {
+      const btnInfo = document.getElementById("explanation");
+      btnInfo.style.display = "block";
+    }
+  };
+
+  // const btnInfo = document.getElementById.("#explanation");
+  // btnInfo.addEventListener.onload("click", function (event) {
+  //   return (
+  //     <div>
+  //       <h4>{nasaData.explanation}</h4>
+  //     </div>
+  //   );
+  // });
+
+  // function btnMaker(object){
+  //   const parentDiv = document.createElement(".information container");
+  //   const infoHeader = document.createElement("h4")
+
+  // }
+
   return (
     <div className="nasa-info">
       {/* <Header title = {nasaData.title}/> */}
-      <h1>NASA Photo of the Day</h1>
-      <h2> Title: {nasaData.title} </h2>
-      <h3> Date Captured: {nasaData.date}</h3>
-      <img src={nasaData.url} alt="photo" />
-      <h4>Photo Information Below</h4>
-      <p> {nasaData.explanation} </p>
+      <div className="header">
+        <h1>NASA Photo of the Day</h1>
+        <h2> Title: {nasaData.title} </h2>
+        <h3> Date Captured: {nasaData.date}</h3>
+      </div>
+      <div className="img-container">
+        <img src={nasaData.url} alt="" />
+      </div>
+      <div className="information container">
+        <h4>Photo Information Below</h4>
+        <button
+          className="button"
+          onClick={() => <h4>{nasaData.explanation}</h4>}
+        >
+          Click for photo information
+        </button>
+        <h4 id="explanation">{nasaData.explanation}</h4>
+      </div>
     </div>
   );
 }
